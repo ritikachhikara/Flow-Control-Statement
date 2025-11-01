2c218d2fa23a0b163100917f1589669c7fbaa2b6
Ritika <riyachhikara26@gmail.com>
2025-11-01 12:18:59 +0530

    Changes

--- Biggest.java
+++ Biggest.java
@@ -1,30 +1,27 @@
+
 import java.util.Scanner;
 
-class Biggest
-{
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the 1st Number: ");
-		int a=sc.nextInt();
-		System.out.println("Enter the 2nd Number: ");
-		int b=sc.nextInt();
-		System.out.println("Enter the 3rd Number: ");
-		int c=sc.nextInt();
-		if(a==b && a==c)
-			System.out.println("All Are Equal");
-		else if(a>b && a>c)
-			System.out.println("Biggest value is: "+a);
-		else if(b>c)
-			System.out.println("Biggest value is: "+b);
-		else
-			System.out.println("Biggest value is: "+c);
-		System.out.println("=====Program Ends=====");
-		
-	}
-	
+class Biggest {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 1st Number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the 2nd Number: ");
+        int b = sc.nextInt();
+        System.out.println("Enter the 3rd Number: ");
+        int c = sc.nextInt();
+        if (a == b && a == c) {
+            System.out.println("All Are Equal"); 
+        }else if (a > b && a > c) {
+            System.out.println("Biggest value is: " + a); 
+        }else if (b > c) {
+            System.out.println("Biggest value is: " + b); 
+        }else {
+            System.out.println("Biggest value is: " + c);
+        }
+        System.out.println("=====Program Ends=====");
+
+    }
+
 }
-
-
-
-
--- ProfitOrLoss.java
+++ ProfitOrLoss.java
@@ -1,36 +1,29 @@
+
 import java.util.Scanner;
 
-class ProfitOrLoss
-{
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the Cost Price: ");
-		double cp=sc.nextDouble();
-		System.out.println("Enter the Selling Price: ");
-		double sp=sc.nextDouble();
-		if(sp>cp)
-		{
-			System.out.println("There is Profit in the transaction");
-			double profit=sp-cp;
-			double proPer=(profit*100)/cp;
-			System.out.println("Profit% is: "+proPer+"%");
-		}
-		else if(cp>sp)
-		{
-			System.out.println("There is Loss in the transaction");
-			double loss=cp-sp;
-			double lossPer=(loss*100)/cp;
-			System.out.println("Loss% is: "+lossPer+"%");
-		}
-		else
-			System.out.println("There is No Profit No Loss in the transaction");
-		System.out.println("=====Program Ends=====");
-		
-	}
-	
+class ProfitOrLoss {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Cost Price: ");
+        double cp = sc.nextDouble();
+        System.out.println("Enter the Selling Price: ");
+        double sp = sc.nextDouble();
+        if (sp > cp) {
+            System.out.println("There is Profit in the transaction");
+            double profit = sp - cp;
+            double proPer = (profit * 100) / cp;
+            System.out.println("Profit% is: " + proPer + "%");
+        } else if (cp > sp) {
+            System.out.println("There is Loss in the transaction");
+            double loss = cp - sp;
+            double lossPer = (loss * 100) / cp;
+            System.out.println("Loss% is: " + lossPer + "%");
+        } else {
+            System.out.println("There is No Profit No Loss in the transaction");
+        }
+        System.out.println("=====Program Ends=====");
+
+    }
+
 }
-
-
-
-
